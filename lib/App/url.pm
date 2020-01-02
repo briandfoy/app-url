df78beb327e4ae584f0fa93ec1468f7cca2dab5f
brian d foy <brian.d.foy@gmail.com>
2020-01-02 02:42:07 -0500

    Created by dist_cooker

---
+++ lib/App/url.pm
@@ -0,0 +1,66 @@
+package App::url;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+App::url - format a URL according to a sprintf-like template
+
+=head1 SYNOPSIS
+
+	use App::url;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/app-url
+
+=head1 AUTHOR
+
+Frank Serpico, C<< <serpico@example.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+
+Copyright (c) 2020, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
